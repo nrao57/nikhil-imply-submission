71fc01611da7fa3bd0c7c2d568261cfecba2f108
nrao57 <raonikhil57@gmail.com>
2024-05-26 12:02:15 -0700

    Successfully loading data from files to bigquery

--- README.md
+++ README.md
@@ -11,10 +11,27 @@ I will be solving this with a single python script using Google BigQuery
 ## Steps
 
 1. Create BQ dataset
-2. Create 3 tables for each file
-3. Import data into respective table
-4. Create view to deduplicate
+2. Create a raw table to store data from files
+3. Load the CSV and Avro data directly from the file
+4. Load the json data from in memory
+5. Create view to deduplicate the raw data
 5. Export view to CSV in Google Cloud Storage
 
+
+## How to Run the Python Script
+1. Install the dependencies with `pip install -r requirements.txt`
+2. Set your google project with `gcloud config set project PROJECT_ID`
+3. Set the environment variable `PROJECT_ID` (with bash it is `export PROJECT_ID=your-project-id`)
+4. run `python solution.py`
+
+
+
+## Questions and Answers
+
+
+## Possible Improvements
+
+1. Partition Table
+2. Use Spark and Cloud Blob Store to Load
 
 
---
+++ solution.py
@@ -0,0 +1,110 @@
+from google.cloud import bigquery
+import os
+import logging
+import json
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def create_dataset(client, dataset_id="nikhil_submission"):
+    """
+    This function creates a dataset for the City List Tables
+    """
+    logging.info("Creating BigQuery Dataset")
+    # Define the dataset ID (replace 'your-project-id' and 'your_dataset_id' accordingly)
+    dataset_ref = client.dataset(dataset_id)
+
+    # Define the dataset
+    dataset = bigquery.Dataset(dataset_ref)
+
+    # Optional: Set properties for the dataset (e.g., location)
+    dataset.location = "US"  # You can set the location to your preference
+
+    # Create the dataset
+    # Set exists_ok=True to not raise an error if the dataset already exists
+    dataset = client.create_dataset(dataset, exists_ok=True)
+
+    logging.info(f"Created dataset {client.project}.{dataset.dataset_id}")
+
+
+def create_table(project_id, client, dataset_id="nikhil_submission", table_name="city_list_raw"):
+    """
+    This function creates the City List Raw Table
+    """
+    logging.info("Creating City List Raw Table")
+    schema = [
+        bigquery.SchemaField("name", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("Population", "INT64", mode="NULLABLE"),
+        bigquery.SchemaField("CountryCode", "STRING", mode="NULLABLE"),
+    ]
+    # Replace 'your_table_id' with your table ID
+    table_id = f"{project_id}.{dataset_id}.{table_name}"
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table, exists_ok=True)
+    logging.info(f"Successfully Created table {table_name}")
+
+
+def load_csv_data(client, filepath, project_id, dataset_id="nikhil_submission", table_name='city_list_raw'):
+    """
+    This function loads a csv file into a bigquery table
+    """
+    logging.info(f"importing the csv file {filepath}")
+    # Load Avro data into BigQuery
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True
+    )
+    table_id = f"{project_id}.{dataset_id}.{table_name}"
+    with open(filepath, "rb") as source_file:
+        load_job = client.load_table_from_file(
+            source_file, table_id, job_config=job_config)
+    load_job.result()
+    logging.info("Finished importing the csv file")
+
+
+def load_json_data(client, filepath, project_id, dataset_id="nikhil_submission", table_name='city_list_raw'):
+    """
+    This function loads a json file into a bigquery table
+    Because the json file is not newline delimited, it must be loaded from memory
+    """
+    logging.info(f"importing the json file {filepath}")
+    # Load Avro data into BigQuery
+    table_id = f"{project_id}.{dataset_id}.{table_name}"
+    with open(filepath, "r") as source_file:
+        data = json.load(source_file)
+    errors = client.insert_rows_json(table_id, data)  # Make an API request.
+    if errors == []:
+        logging.info(f"New rows have been added to {table_id}.")
+    else:
+        logging.error(f"Encountered errors while inserting rows: {errors}")
+    logging.info("Finished importing the json file")
+    pass
+
+
+def load_avro_data(client, filepath, project_id, dataset_id="nikhil_submission", table_name='city_list_raw'):
+    """
+    This function loads an avro file into a bigquery table
+    """
+    logging.info(f"importing the avro file {filepath}")
+    # Load Avro data into BigQuery
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.AVRO
+    )
+    table_id = f"{project_id}.{dataset_id}.{table_name}"
+    with open(filepath, "rb") as source_file:
+        load_job = client.load_table_from_file(
+            source_file, table_id, job_config=job_config)
+    load_job.result()
+    logging.info("Finished importing the avro file")
+
+
+if __name__ == "__main__":
+    project_id = os.getenv('PROJECT_ID')
+    client = bigquery.Client()
+    create_dataset(client)
+    create_table(project_id, client)
+    load_avro_data(client, 'CityListB.avro', project_id)
+    load_csv_data(client, 'CityList.csv', project_id)
+    load_json_data(client, 'CityListA.json', project_id)
